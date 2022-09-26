25d212794c39dc18f9faebfd60a1cee110f6f29a
itzish1999 <ismaeel.butt19@gmail.com>
2022-09-26 12:53:13 -0400

    feat(Strict Mode) explained

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,19 @@
+/*
+// Strict mode helps us avoid making bugs. To inoke strict mode, use the following
+'use strict';
+
+// Important changes strict mode introduces
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true; // lets say I mispelled hasDriversLicense. Strict mode will tell us where the bug is and the error
+
+if (hasDriversLicense) console.log("I can drive :D!");
+
+
+// Strict mode also has reserved words that we can't use. Here are a few examples
+const interface = 'Audio';
+const private = 534;
+*/
+
